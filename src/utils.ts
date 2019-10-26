bd21ba4ea891201ef4385e245377161c4cbfba68
Dirk Van Haerenborgh <vhdirk@gmail.com>
2019-10-26 14:33:24 +0200

    update dependencies

--- src/utils.ts
+++ src/utils.ts
@@ -2,9 +2,9 @@ import * as tmp from 'tmp';
 import * as fs from 'fs';
 import * as promisify from 'pify';
 
-declare const tmp: {
-    file: (options: { [key: string]: string }, cb: (err, path) => void) => void
-};
+// declare const tmp: {
+//     file: (options: { [key: string]: string }, cb: (err, path) => void) => void
+// };
 
 type TmpFileFunc = (options: { [key: string]: string }) => Promise<string>;
 export const tmpFile: TmpFileFunc = promisify(tmp.file);
